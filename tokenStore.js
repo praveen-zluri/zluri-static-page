5402ad658b1a028f52cb8be551b1877ee7f10149
praveenk <praveen.akrity@zluri.com>
2022-10-12 19:00:02 +0530

    Static page redirection

--- tokenStore.js
+++ tokenStore.js
@@ -1,36 +1,44 @@
 const url = window.location.search
 const urlParams = new URLSearchParams(url)
 
-let browser = urlParams.get('b');
-
+let intent = urlParams.get('intent');
 let UUID, org_token, mdm_type;
-
 
 UUID = urlParams.get('UUID')
 org_token = urlParams.get('org_token')
 mdm_type = urlParams.get('mdm_type')
 
-var mdm_args = {};
-mdm_args = {
-    UUID,
-    org_token,
-    mdm_type
-}
+console.log(intent);
 
+if(intent) {
+  if(intent == "getArgs") {
+    let args = JSON.parse(localStorage.getItem('mdm_args'));
+    console.log(`args:`, args);
+    
+    UUID=args.UUID;
+    org_token=args.org_token;
+    mdm_type=args.mdm_type;
 
- if(browser) {
-  if(browser == 'c') {
-    let token = JSON.parse(localStorage.getItem('chrome_token'))
-    if(token) {
-      window.location.replace(`chrome-extension://${process.env.CHROME_2}/options.html?token=${JSON.stringify(token)}`)
+    window.location.href = `chrome-extension://cckofokgndiepohkhjnohjcmjekjeppg/options.html?UUID=${UUID}&org_token=${org_token}&mdm_type=${mdm_type}`;
+    /* if(token) {
+      window.location.replace(`chrome-extension://cckofokgndiepohkhjnohjcmjekjeppg/options.html?token=${JSON.stringify(token)}`)
     }
   } else {
     console.log(process.env.CHROME_2)
-  }
+  } */
+}
 }
 
+if(UUID && org_token && mdm_type) {
+  
+  var mdm_args = {};
 
+  mdm_args = {
+      UUID,
+      org_token,
+      mdm_type
+  }
 
 localStorage.setItem('mdm_args',JSON.stringify(mdm_args));
-// localStorage.setItem('firefox_token',JSON.stringify(mdm_args.firefox))
-// localStorage.setItem('edge_token',JSON.stringify(mdm_args.edge))
+
+}
