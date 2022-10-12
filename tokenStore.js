c50b576442aba657cd993d3c8ca01e33367741b5
praveenk <praveen.akrity@zluri.com>
2022-10-12 19:39:05 +0530

    Completing the flow

--- tokenStore.js
+++ tokenStore.js
@@ -4,11 +4,9 @@ const urlParams = new URLSearchParams(url)
 let intent = urlParams.get('intent');
 let UUID, org_token, mdm_type;
 
-UUID = urlParams.get('UUID')
-org_token = urlParams.get('org_token')
-mdm_type = urlParams.get('mdm_type')
-
-console.log(intent);
+UUID = urlParams.get('UUID');
+org_token = urlParams.get('org_token');
+mdm_type = urlParams.get('mdm_type');
 
 if(intent) {
   if(intent == "getArgs") {
@@ -20,13 +18,7 @@ if(intent) {
     mdm_type=args.mdm_type;
 
     window.location.href = `chrome-extension://cckofokgndiepohkhjnohjcmjekjeppg/options.html?UUID=${UUID}&org_token=${org_token}&mdm_type=${mdm_type}`;
-    /* if(token) {
-      window.location.replace(`chrome-extension://cckofokgndiepohkhjnohjcmjekjeppg/options.html?token=${JSON.stringify(token)}`)
-    }
-  } else {
-    console.log(process.env.CHROME_2)
-  } */
-}
+  }
 }
 
 if(UUID && org_token && mdm_type) {
@@ -40,5 +32,7 @@ if(UUID && org_token && mdm_type) {
   }
 
 localStorage.setItem('mdm_args',JSON.stringify(mdm_args));
+
+window.location.href = `https://zluri-static-page.vercel.app/static.html?intent=getArgs`
 
 }
