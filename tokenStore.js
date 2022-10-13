8939745b0cfdf74157fad7fcfdd0a618a5191eb4
praveenk <praveen.akrity@zluri.com>
2022-10-14 00:45:36 +0530

    getting browser from localstorage

--- tokenStore.js
+++ tokenStore.js
@@ -1,7 +1,7 @@
 //https://zluri-static-page.vercel.app/static.html?browser=chrome&UUID=5c1bb6da-dfb7-4940-b5a6-d82c9b13c92d&org_token=org87e2369r7ndew7r3yo&mdm_type=mdm-mac-kandji
 
 const CHROME_EXT = "cckofokgndiepohkhjnohjcmjekjeppg"
-const FIREFOX_EXT = "cckofokgndiepohkhjnohjcmjekjeppga4f8718a-1f20-4d97-a740-ffb134e4ade4"
+const FIREFOX_EXT = "4f8718a-1f20-4d97-a740-ffb134e4ade4"
 
 const url = window.location.search
 const urlParams = new URLSearchParams(url)
@@ -9,23 +9,26 @@ const urlParams = new URLSearchParams(url)
 let intent = urlParams.get('intent');
 let UUID, org_token, mdm_type, browser;
 
-browser = urlParams.get('browser');
+
+
 UUID = urlParams.get('UUID');
 org_token = urlParams.get('org_token');
 mdm_type = urlParams.get('mdm_type');
-
+browser = urlParams.get('browser');
 
 if(UUID && org_token && mdm_type) {
-  storeArgsToLocalStorage(browser, UUID, org_token, mdm_type)
+  localStorage.setItem("browser",browser);
+  storeArgsToLocalStorage(UUID, org_token, mdm_type);
 }
 
 
 if(intent && intent === "getArgs") {
+  let browser = localStorage.getItem("browser");
   if(isArgsInLocalStorage()) {
   let savedArgs = getArgsFromLocalStorage();
   //if args found
    
-  browser = savedArgs.browser;
+  loca = savedArgs.browser;
   UUID = savedArgs.UUID;
   org_token = savedArgs.org_token;
   mdm_type = savedArgs.mdm_type;
