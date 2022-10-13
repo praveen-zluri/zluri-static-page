791366a1be3bde6d873808ebd3a64625abad326b
praveenk <praveen.akrity@zluri.com>
2022-10-14 00:32:27 +0530

    Condtional rendering based on the args presence

--- tokenStore.js
+++ tokenStore.js
@@ -1,5 +1,8 @@
 //https://zluri-static-page.vercel.app/static.html?browser=chrome&UUID=5c1bb6da-dfb7-4940-b5a6-d82c9b13c92d&org_token=org87e2369r7ndew7r3yo&mdm_type=mdm-mac-kandji
 
+const CHROME_EXT = "cckofokgndiepohkhjnohjcmjekjeppg"
+const FIREFOX_EXT = "cckofokgndiepohkhjnohjcmjekjeppga4f8718a-1f20-4d97-a740-ffb134e4ade4"
+
 const url = window.location.search
 const urlParams = new URLSearchParams(url)
 
@@ -19,15 +22,21 @@ if(UUID && org_token && mdm_type) {
 
 if(intent && intent === "getArgs") {
   if(isArgsInLocalStorage()) {
-   let savedArgs = getArgsFromLocalStorage();
+  let savedArgs = getArgsFromLocalStorage();
+  //if args found
+   
   browser = savedArgs.browser;
   UUID = savedArgs.UUID;
   org_token = savedArgs.org_token;
   mdm_type = savedArgs.mdm_type;
   
-   browser === "chrome" ? window.location.href = `chrome-extension://cckofokgndiepohkhjnohjcmjekjeppg/options.html?UUID=${UUID}&org_token=${org_token}&mdm_type=${mdm_type}`
-   : location.href = `moz-extension://a4f8718a-1f20-4d97-a740-ffb134e4ade4/options.html?UUID=${UUID}&org_token=${org_token}&mdm_type=${mdm_type}`;
-   
-  // window.location.href = `chrome-extension://cckofokgndiepohkhjnohjcmjekjeppg/options.html?UUID=${UUID}&org_token=${org_token}&mdm_type=${mdm_type}`;
+  if(savedArgs)
+   browser === "chrome" ? window.location.href = `chrome-extension://${CHROME_EXT}/options.html?UUID=${UUID}&org_token=${org_token}&mdm_type=${mdm_type}`
+   : window.open(`moz-extension://${FIREFOX_EXT}/options.html?UUID=${UUID}&org_token=${org_token}&mdm_type=${mdm_type}`);
+
+   //if  args not found
+   else
+    browser === "chrome" ? window.location.href = `chrome-extension://${CHROME_EXT}/options.html`
+   : window.open(`moz-extension://${FIREFOX_EXT}/options.html`);
   }
 }
