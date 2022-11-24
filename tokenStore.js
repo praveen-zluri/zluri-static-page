0525afb07d9e5f6be9e8893a00be0d3e370131b3
praveenk <praveen.akrity@zluri.com>
2022-11-24 14:41:18 +0530

    Handling extension redirection for different browsers

--- tokenStore.js
+++ tokenStore.js
@@ -1,6 +1,8 @@
 //https://zluri-static-page.vercel.app/static.html?browser=chrome&UUID=5c1bb6da-dfb7-4940-b5a6-d82c9b13c92d&org_token=org87e2369r7ndew7r3yo&mdm_type=mdm-mac-kandji
 
 const CHROME_EXT = "cmobkdiplndgpjodaioofofmcikimbdb"
+// const EDGE_EXT = "llnpohinpfhpnjbfcnmkjfccaengcffb"
+const EDGE_EXT = "cmobkdiplndgpjodaioofofmcikimbdb"
 const FIREFOX_EXT = "4f8718a-1f20-4d97-a740-ffb134e4ade4"
 
 const url = window.location.search
@@ -35,13 +37,14 @@ if(intent && intent === "getArgs") {
   //if args found
   if(savedArgs)
    if(browser == "chrome") window.location.href = `chrome-extension://${CHROME_EXT}/options.html?UUID=${UUID}&org_token=${org_token}&mdm_type=${mdm_type}`
-   else window.location.href=`moz-extension://0716deb9-8d5d-44a9-a963-11ff590c42cc/options.html?UUID=${UUID}&org_token=${org_token}&mdm_type=${mdm_type}`;
+   else if (browser == "edge-chromium") window.location.href = `chrome-extension://${EDGE_EXT}/options.html?UUID=${UUID}&org_token=${org_token}&mdm_type=${mdm_type}`
+   else if(browser === "firefox") window.location.href=`moz-extension://0716deb9-8d5d-44a9-a963-11ff590c42cc/options.html?UUID=${UUID}&org_token=${org_token}&mdm_type=${mdm_type}`;
 
    //if  args not found
        
   } else {
-    // if(browser == "chrome") 
+    if(browser == "chrome" || browser == "edge-chromium")
     window.location.href = `chrome-extension://${CHROME_EXT}/options.html`
-    // else window.location.href=`moz-extension://2855a1e1-9197-44fe-9da4-c73404d54d74/options.html`;
+    else window.location.href=`moz-extension://2855a1e1-9197-44fe-9da4-c73404d54d74/options.html`;
   }
 }
